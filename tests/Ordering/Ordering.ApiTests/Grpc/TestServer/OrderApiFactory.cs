3910e35136ef5e2ff47bbf6ce3816adf00b99132
Kiyomarss <kiyomarss.p1997@gmail.com>
2025-11-17 19:00:40 +0330

    جا به جایی فولدر

--- tests/Ordering/Ordering.ApiTests/Grpc/TestServer/OrderApiFactory.cs
+++ tests/Ordering/Ordering.ApiTests/Grpc/TestServer/OrderApiFactory.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Ordering.ApiTests.Utilities;
 
 namespace Ordering.ApiTests.Grpc.TestServer;
