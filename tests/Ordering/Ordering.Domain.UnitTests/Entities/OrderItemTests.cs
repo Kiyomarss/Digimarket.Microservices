510e96b5b27b989a19b85b533d7f330ba6c5a487
Kiyomarss <kiyomarss.p1997@gmail.com>
2025-11-21 22:01:38 +0330

    افزودن پروژه Ordering.Domain.UnitTests

---
+++ tests/Ordering/Ordering.Domain.UnitTests/Entities/OrderItemTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Ordering_Domain.Domain.Entities;
+
+namespace Ordering.Domain.UnitTests.Entities;
+
+public class OrderItemTests
+{
+    [Fact]
+    public void Parameterless_Constructor_Should_Create_Instance_For_EF()
+    {
+        // Act
+        var item = new OrderItem();
+
+        // Assert
+        item.Id.Should().NotBe(Guid.Empty);
+        item.Price.Should().Be(0);
+        item.Quantity.Should().Be(0);
+        item.ProductName.Should().BeNullOrEmpty();
+    }
+
+    [Fact]
+    public void Constructor_With_Parameters_Should_Set_Properties()
+    {
+        // Arrange
+        var productId = Guid.NewGuid();
+
+        // Act
+        var item = new OrderItem(productId, "Test Product", 5, 999);
+
+        // Assert
+        item.ProductId.Should().Be(productId);
+        item.ProductName.Should().Be("Test Product");
+        item.Quantity.Should().Be(5);
+        item.Price.Should().Be(999);
+    }
+}
---
+++ tests/Ordering/Ordering.Domain.UnitTests/Entities/OrderTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Ordering_Domain.Domain.Entities;
+
+namespace Ordering.Domain.UnitTests.Entities;
+
+public class OrderTests
+{
+    [Fact]
+    public void New_Order_Should_Have_Default_Values()
+    {
+        // Act
+        var order = new Order();
+
+        // Assert
+        order.Id.Should().NotBe(Guid.Empty);
+        order.Date.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+        order.State.Should().Be("Init");
+        order.Items.Should().BeEmpty();
+        order.Customer.Should().BeNull();
+        order.TotalPrice.Should().Be(0);
+    }
+
+    [Fact]
+    public void TotalPrice_Should_Be_Sum_Of_Item_Price_x_Quantity()
+    {
+        // Arrange
+        var order = new Order
+        {
+            Customer = "Ali",
+            Items =
+            {
+                new OrderItem { Price = 1000, Quantity = 2 }, // 2000
+                new OrderItem { Price = 500, Quantity = 3 }   // 1500
+            }
+        };
+
+        // Act
+        var total = order.TotalPrice;
+
+        // Assert
+        total.Should().Be(3500);
+    }
+
+    [Fact]
+    public void Adding_OrderItem_Should_Update_TotalPrice()
+    {
+        // Arrange
+        var order = new Order();
+        var item = new OrderItem
+        {
+            Price = 1000,
+            Quantity = 3
+        };
+
+        // Act
+        order.Items.Add(item);
+
+        // Assert
+        order.TotalPrice.Should().Be(3000);
+    }
+}
