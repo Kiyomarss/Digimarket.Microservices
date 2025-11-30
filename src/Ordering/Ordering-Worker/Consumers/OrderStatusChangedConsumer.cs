a522d49d2cd32a7c98fb7d4cec7fe051dde0c5e5
Kiyomarss <kiyomarss.p1997@gmail.com>
2025-11-30 19:30:13 +0330

    حذف کد اضافه

--- src/Ordering/Ordering-Worker/Consumers/OrderStatusChangedConsumer.cs
+++ src/Ordering/Ordering-Worker/Consumers/OrderStatusChangedConsumer.cs
@@ -18,18 +18,7 @@ namespace Ordering.Worker.Consumers
         public async Task Consume(ConsumeContext<OrderStatusChanged> context)
         {
             var message = context.Message;
-
-            var a = _orderRepository.FindOrderById(Guid.Parse("9336d6b2-68cf-48f8-81c0-5eb335f4571e"));
-            var order = new Order();
-
-            if (order == null)
-            {
-                _logger.LogWarning("Order with ID {OrderId} not found when trying to update status to {NewStatus}", 
-                                   message.Id, message.OrderState);
-                return;
-            }
-
-            order.State = message.OrderState;
+            
             await _unitOfWork.SaveChangesAsync();
         }
     }
