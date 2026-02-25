97dffd011d5016a284e612fe38ee43fa091f82d6
Kiyomarss <kiyomarss.p1997@gmail.com>
2026-02-25 07:33:55 +0330

    Add Stock In Catalog

---
+++ src/Catalog/Catalog.Infrastructure/Migrations/20260225035720_AddStock.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Stock",
+                table: "product",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+            
+            migrationBuilder.Sql("""
+                                     UPDATE product
+                                     SET "Stock" = 10
+                                 """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stock",
+                table: "product");
+        }
+    }
+}
