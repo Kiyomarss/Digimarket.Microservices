c6d20e56fe174c3d2b917220e0dda971da36d8b1
Kiyomarss <kiyomarss.p1997@gmail.com>
2026-05-21 21:55:38 +0330

    بهبود در ساختار AuthorizationController

--- src/Identity2/Identity.Api/Controllers/AuthorizationController.cs
+++ src/Identity2/Identity.Api/Controllers/AuthorizationController.cs
@@ -32,67 +32,76 @@ public class AuthorizationController : Controller
         if (request is null)
             throw new InvalidOperationException("OpenIddict request cannot be retrieved.");
 
-        if (request.IsPasswordGrantType())
+        if (!request.IsPasswordGrantType())
+            throw new NotSupportedException("The specified grant type is not supported.");
+
+        var user = await _userRepository.GetByEmailAsync(
+                                                         request.Username!,
+                                                         HttpContext.RequestAborted);
+
+        if (user is null)
+            return Forbid(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+        var validPassword = _passwordHasher.VerifyPassword(
+                                                           request.Password!,
+                                                           user.PasswordHash);
+
+        if (!validPassword)
+            return Forbid(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+        var identity = new ClaimsIdentity(
+                                          OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+        identity.AddClaim(OpenIddictConstants.Claims.Subject, user.Id.ToString());
+        identity.AddClaim(OpenIddictConstants.Claims.Email, user.Email);
+        identity.AddClaim(OpenIddictConstants.Claims.Name, user.Email);
+
+        foreach (var role in user.Roles)
         {
-            var user = await _userRepository.GetByEmailAsync(
-                                                             request.Username!,
-                                                             HttpContext.RequestAborted);
-
-            if (user is null)
-                return Forbid(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
-
-            var isValidPassword = _passwordHasher.VerifyPassword(
-                                                                 request.Password!,
-                                                                 user.PasswordHash);
-
-            if (!isValidPassword)
-                return Forbid(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
-
-            var identity = new ClaimsIdentity(
-                                              OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
-
-            identity.AddClaim(OpenIddictConstants.Claims.Subject, user.Id.ToString());
-            identity.AddClaim(OpenIddictConstants.Claims.Email, user.Email);
-            identity.AddClaim(OpenIddictConstants.Claims.Name, user.Email);
-
-            foreach (var role in user.Roles)
-            {
-                identity.AddClaim(OpenIddictConstants.Claims.Role, role.Role.Name);
-            }
-            
-            foreach (var claim in user.Claims)
-            {
-                identity.AddClaim(claim.Type, claim.Value);
-            }
-            
-            var principal = new ClaimsPrincipal(identity);
-
-            foreach (var claim in principal.Claims)
-            {
-                claim.SetDestinations(OpenIddictConstants.Destinations.AccessToken);
-            }
-
-            var scopes = request.GetScopes();
-
-            principal.SetScopes(scopes);
-
-            var audiences = scopes
-                            .Where(s => ScopeAudienceMap.ContainsKey(s))
-                            .Select(s => ScopeAudienceMap[s])
-                            .Distinct()
-                            .ToList();
-
-            principal.SetResources(audiences);
-            principal.SetAudiences(audiences);
-            
-            return SignIn(
-                          principal,
-                          OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+            identity.AddClaim(OpenIddictConstants.Claims.Role, role.Role.Name);
         }
 
-        throw new NotImplementedException("Grant type is not supported.");
+        foreach (var claim in user.Claims)
+        {
+            identity.AddClaim(claim.Type, claim.Value);
+        }
+
+        var principal = new ClaimsPrincipal(identity);
+
+        // scopes requested by client
+        var scopes = request.GetScopes();
+
+        principal.SetScopes(scopes);
+
+        var audiences = scopes
+                        .Where(s => ScopeAudienceMap.ContainsKey(s))
+                        .Select(s => ScopeAudienceMap[s])
+                        .Distinct()
+                        .ToList();
+
+        principal.SetResources(audiences);
+        principal.SetAudiences(audiences);
+
+        // claim filtering (مهم در production)
+        principal.SetDestinations(claim => claim.Type switch
+        {
+            OpenIddictConstants.Claims.Name
+                or OpenIddictConstants.Claims.Email
+                or OpenIddictConstants.Claims.Role
+                or OpenIddictConstants.Claims.Subject
+                => [OpenIddictConstants.Destinations.AccessToken],
+
+            "permission"
+                => [OpenIddictConstants.Destinations.AccessToken],
+
+            _ => []
+        });
+
+        return SignIn(
+                      principal,
+                      OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
     }
-    
+
     private static readonly Dictionary<string, string> ScopeAudienceMap =
         new()
         {
