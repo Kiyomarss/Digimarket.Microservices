bdffcacd9f6045290a06bcf992b8aae49023e359
Kiyomarss <kiyomarss.p1997@gmail.com>
2026-06-20 23:24:53 +0330

    Add-Migration AddCancelledAfterPaymentState

---
+++ src/Ordering/Ordering.Infrastructure/Migrations/20260620185812_AddCancelledAfterPaymentState.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCancelledAfterPaymentState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                     UPDATE orders
+                                     SET order_state_id = order_state_id + 1
+                                     WHERE order_state_id >= 4;
+                                 """);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                     UPDATE orders
+                                     SET order_state_id = order_state_id - 1
+                                     WHERE order_state_id >= 5;
+                                 """);
+        }
+    }
+}
